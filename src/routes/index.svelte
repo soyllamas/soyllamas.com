9c53b21e9a178bb1b547caaca6d7bb87ca4194e9
Daniel Llamas <daniel@soyllamas.com>
2021-11-24 23:31:14 -0600

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+	<title>Daniel Llamas</title>
+</svelte:head>
+
+<h1>Daniel Llamas</h1>
+<h2>Android Engineer</h2>
+
+<style>
+
+    :global(*) {
+        margin: 0;
+        padding: 0;
+    }
+
+    :global(body) {
+        font-family: system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji
+    }
+
+    h1 {
+        color: #FF6673;
+        font-size: 24px;
+        text-align: center;
+        margin-top: 72px;
+        font-weight: 700;
+        margin-bottom: 2px;
+    }
+
+    h2 {
+        font-size: 16px;
+        font-weight: 400;
+        text-align: center;
+    }
+
+</style>

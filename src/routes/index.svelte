85fca35bccd8c8d5f53ccd206ca44384cae8b4b2
Daniel Llamas <daniel@soyllamas.com>
2021-11-25 21:54:20 -0600

    Add cover, favicon & finish CV

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,107 @@
 <svelte:head>
-	<title>Daniel Llamas</title>
+	<!-- Primary Meta Tags -->
+	<title>Daniel Llamas - Android Engineer</title>
+	<meta name="title" content="Daniel Llamas - Android Engineer">
+	<meta name="description" content="Inquisitive human being. Passionate creator of high-quality human-centered products. Bringing humanity to technology. Android Engineer @ Wizeline.">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://soyllamas.com/">
+	<meta property="og:title" content="Daniel Llamas - Android Engineer">
+	<meta property="og:description" content="Inquisitive human being. Passionate creator of high-quality human-centered products. Bringing humanity to technology. Android Engineer @ Wizeline.">
+	<meta property="og:image" content="/cover.png">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://soyllamas.com/">
+	<meta property="twitter:title" content="Daniel Llamas - Android Engineer">
+	<meta property="twitter:description" content="Inquisitive human being. Passionate creator of high-quality human-centered products. Bringing humanity to technology. Android Engineer @ Wizeline.">
+	<meta property="twitter:image" content="/cover.png">
 </svelte:head>
 
 <h1>Daniel Llamas</h1>
 <h2>Android Engineer</h2>
+
+<main>
+	<section>
+		<h3>Work Experience</h3>
+		<div>
+			<p class="title">Software Engineer, Android - Wizeline</p>
+			<p class="subtitle">January 2021 - Present</p>
+			<ul>
+				<li>Design and develop advanced applications for Android that are used by millions of users across the
+					world.
+				</li>
+				<li>Make confident architecture and UX decisions based on skill, experience, and research. Ensure
+					high-quality products implementing SOLID Principles, Design Patterns and Clean Architecture.
+				</li>
+			</ul>
+		</div>
+		<div class="entry">
+			<p class="title">Co-founder & CTO - Nivélate</p>
+			<p class="subtitle">November 2015 - January 2021</p>
+			<ul>
+				<li>Managed a cross-functional team of 14 people conformed of marketers, developers, designers and
+					academics.
+				</li>
+				<li>Implemented OKRs & Agile for the whole company reducing our monthly meetings by 60% while improving
+					productivity by 45%.
+				</li>
+				<li>Created timelines for the development and deployment of our technologies.</li>
+				<li>Provided the development team with architectural blueprints to follow.</li>
+				<li>Ensured all technology practices adhere to industry standards.</li>
+			</ul>
+		</div>
+		<div class="entry">
+			<p class="title">Mobile Software Developer - Brounie</p>
+			<p class="subtitle">January 2018 - February 2019</p>
+			<ul>
+				<li>Design and develop advanced applications for Android that are used by millions of users across the
+					world.
+				</li>
+				<li>Make confident architecture and UX decisions based on skill, experience, and research. Ensure
+					high-quality products implementing SOLID Principles, Design Patterns and Clean Architecture.
+				</li>
+			</ul>
+		</div>
+	</section>
+	<section>
+		<h3>Education</h3>
+		<div>
+			<p class="title">Universidad Utel - Computer Science Engineering</p>
+			<p class="subtitle">July 2020 - October 2022</p>
+		</div>
+		<div class="entry">
+			<p class="title">Tecnológico de Monterrey - Computer Science Engineering</p>
+			<p class="subtitle">August 2016 - February 2018</p>
+		</div>
+	</section>
+	<section>
+		<h3>Certifications</h3>
+		<div>
+			<p class="title">Android Mobile Apprenticeship Program - Wizeline Academy</p>
+			<p class="subtitle">Issued February 2021</p>
+		</div>
+		<div class="entry">
+			<p class="title">Prof G Certified Brand Strategist - Section4</p>
+			<p class="subtitle">Issued November 2020</p>
+		</div>
+		<div class="entry">
+			<p class="title">Prof G Certified Business Strategist - Section4</p>
+			<p class="subtitle">Issued June 2020</p>
+		</div>
+		<div class="entry">
+			<p class="title">Data Analyst - Udacity</p>
+			<p class="subtitle">Issued March 2019</p>
+		</div>
+		<div class="entry">
+			<p class="title">Data Foundations - Udacity</p>
+			<p class="subtitle">Issued August 2018</p>
+		</div>
+	</section>
+</main>
+
+<p class="footer">with ❤️ from daniel</p>
 
 <style>
 
@@ -29,6 +127,59 @@
         font-size: 16px;
         font-weight: 400;
         text-align: center;
+    }
+
+    h3 {
+        font-size: 36px;
+        margin-bottom: 24px;
+        font-weight: 700;
+    }
+
+    .title {
+        font-size: 18px;
+        font-weight: 500;
+        margin-bottom: 4px;
+        line-height: 1.2em;
+    }
+
+    .subtitle {
+        font-size: 16px;
+        font-weight: 400;
+        opacity: 66%;
+    }
+
+    .footer {
+        font-size: 14px;
+        opacity: 43%;
+        text-align: center;
+        padding-bottom: 24px;
+    }
+
+    ul {
+        margin-top: 12px;
+        margin-left: 20px;
+    }
+
+    li {
+        opacity: 43%;
+        margin-bottom: 8px;
+        line-height: 24px;
+        size: 16px;
+    }
+
+    .entry {
+        margin-top: 32px;
+    }
+
+    main {
+        box-sizing: border-box;
+        margin: 120px auto 240px auto;
+        max-width: 640px;
+        padding: 0 25px;
+    }
+
+    section {
+        margin-bottom: 64px;
     }
 
 </style>

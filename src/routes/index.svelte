29487b01d91198770be7147e339ee4c551d584c5
Daniel Llamas <daniel@soyllamas.com>
2022-08-28 17:18:18 -0500

    Add new hero and favorite books section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,69 +1,90 @@
 <svelte:head>
     <!-- Primary Meta Tags -->
-    <title>Daniel Llamas - Android Developer</title>
-    <meta name="title" content="Daniel Llamas - Android Developer">
+    <title>Daniel Llamas - Android & Flutter Developer</title>
+    <meta name="title" content="Daniel Llamas - Android & Flutter Developer">
     <meta name="description"
-          content="🧠 Inquisitive human being. 🎨 Passionate creator of high-quality human-centered products. 🎉 Bringing humanity to technology through compelling digital experiences. Currently working as Android Developer @ Wizeline.">
+          content="🧠 Inquisitive human being. 🎨 Passionate creator of high-quality human-centered products. 🎉 Bringing humanity to technology through compelling digital experiences. Currently working as Flutter Developer @ Fondeadora.">
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://soyllamas.com/">
-    <meta property="og:title" content="Daniel Llamas - Android Developer">
+    <meta property="og:title" content="Daniel Llamas - Android & Flutter Developer">
     <meta property="og:description"
-          content="🧠 Inquisitive human being. 🎨 Passionate creator of high-quality human-centered products. 🎉 Bringing humanity to technology through compelling digital experiences. Currently working as Android Developer @ Wizeline.">
-    <meta property="og:image" content="https://soyllamas.com/cover.png">
+          content="🧠 Inquisitive human being. 🎨 Passionate creator of high-quality human-centered products. 🎉 Bringing humanity to technology through compelling digital experiences. Currently working as Flutter Developer @ Fondeadora.">
+    <meta property="og:image" content="https://soyllamas.com/cover.jpg">
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://soyllamas.com/">
-    <meta property="twitter:title" content="Daniel Llamas - Android Developer">
+    <meta property="twitter:title" content="Daniel Llamas - Android & Flutter Developer">
     <meta property="twitter:description"
-          content="🧠 Inquisitive human being. 🎨 Passionate creator of high-quality human-centered products. 🎉 Bringing humanity to technology through compelling digital experiences. Currently working as Android Developer @ Wizeline.">
-    <meta property="twitter:image" content="https://soyllamas.com/cover.png">
+          content="🧠 Inquisitive human being. 🎨 Passionate creator of high-quality human-centered products. 🎉 Bringing humanity to technology through compelling digital experiences. Currently working as Flutter Developer @ Fondeadora.">
+    <meta property="twitter:image" content="https://soyllamas.com/cover.jpg">
 </svelte:head>
 
-<h1>Daniel Llamas</h1>
-<h2>Android Developer</h2>
+<nav>
+    <img class="face" src="/face.png"/>
+    <p class="name">Daniel<br/>Llamas</p>
+    <a class="link" href="https://www.linkedin.com/in/soyllamas/">LinkedIn</a>
+    <a class="link" href="https://twitter.com/soyllamas">Twitter</a>
+    <a class="link" href="https://www.instagram.com/soyllamas/">Instagram</a>
+    <a class="link" href="mailto:daniel@soyllamas.com">Get in touch</a>
+</nav>
+
+<section class="hero">
+    <h1 class="summary"><span class="gradient-text">Android & Flutter Developer</span> — 👋🏼 I'm a passionate creator
+        of high-quality human-centered products. Currently working as Flutter developer at <a
+                href="http://fondeadora.com">Fondeadora</a>.</h1>
+</section>
 
 <main>
     <section>
         <h3>Work Experience</h3>
         <div>
-            <p class="title">Software Engineer II, Android - Wizeline</p>
-            <p class="subtitle">January 2021 - Present</p>
+            <a href="https://fondeadora.com"><p class="title">Software Engineer, Flutter — Fondeadora</p></a>
+            <p class="subtitle">April 2022 — Present</p>
+        </div>
+        <div class="entry">
+            <a href="https://wizeline.com"><p class="title">Software Engineer II, Android — Wizeline</p></a>
+            <p class="subtitle">January 2021 — April 2022</p>
             <ul>
-                <li>Migrated an Android Library module from manual Dependency Injection to Android’s recommended
-                    framework, Hilt.
-                </li>
-                <li>Developed an internal project to improve onboarding experience for new hires through a simple and
-                    elegant learning platform.
+                <li>Migrated Walmart's México app from manual dependency injection to Android’s recommended framework,
+                    Hilt. Reducing boilerplate, improving productivity.
                 </li>
                 <li>Participated as an active member of a community which fosters high-quality software development
-                    using SOLID Principles, Design Patters, Clean Architecture, TDD, DDD, to name a few.
+                    using SOLID Principles, Design Patters, Clean Architecture, Test-Driven Development, to name a few.
+                </li>
+                <li>Led and developed an internal project to improve onboarding experience for new hires through a
+                    simple and elegant learning platform.
                 </li>
             </ul>
         </div>
         <div class="entry">
-            <p class="title">Co-founder & CTO - Nivélate</p>
-            <p class="subtitle">November 2015 - January 2021</p>
+            <a href="https://nivelate.mx" target="_blank"><p class="title">Co-founder & CTO — Nivélate</p></a>
+            <p class="subtitle">November 2015 — January 2021</p>
             <ul>
                 <li>Managed a cross-functional team of 14 people conformed of marketers, developers, designers and
-                    academics.
+                    academics using <a href="https://agilemanifesto.org/">Agile principles</a>.
                 </li>
-                <li>Implemented OKRs to define specific goals for the company and individual contributors.
+                <li>Implemented <a href="https://www.whatmatters.com/faqs/okr-meaning-definition-example">OKRs</a> to
+                    define specific goals for the company and individual contributors.
                 </li>
-                <li>Guaranteed high-quality software using SOLID Principles and Clean Architecture.</li>
-                <li>Created timelines for the development and deployment of our technologies.</li>
-                <li>Provided the development team with architectural blueprints to follow.</li>
+                <li>Guaranteed high-quality software by constantly training the development team on SOLID Principles and
+                    Clean Architecture.
+                </li>
+                <li>Clearly aligned each contributor's personal aspirations with business objectives to increase
+                    fulfillment and satisfaction while reaching our goals using <a
+                            href="https://www.whatmatters.com/resources/difference-between-okr-cfr">CFRs</a>.
+                </li>
             </ul>
         </div>
         <div class="entry">
-            <p class="title">Mobile Developer - Brounie</p>
-            <p class="subtitle">January 2018 - February 2019</p>
+            <p class="title">Mobile Developer — Brounie</p>
+            <p class="subtitle">January 2018 — February 2019</p>
             <ul>
-                <li>Developed +5 apps for both Android and iOS mobile platforms.
+                <li>Contributed to the development of 5+ apps for Android and iOS.
                 </li>
-                <li>Mastered MVVM architecture ensuring app quality and performance.
+                <li>Mastered MVVM design pattern ensuring app's quality, stability and performance.
                 </li>
                 <li>Worked closely with customers and UI/UX designers to define app features.
                 </li>
@@ -73,40 +94,63 @@
     <section>
         <h3>Education</h3>
         <div>
-            <p class="title">Universidad Tecnológica Latinoamericana - B.S. Computer Engineering</p>
-            <p class="subtitle">July 2020 - October 2022</p>
+            <p class="title">Universidad Tecnológica Latinoamericana — B.S. Computer Engineering</p>
+            <p class="subtitle">July 2020 — October 2022</p>
         </div>
         <div class="entry">
-            <p class="title">Tecnológico de Monterrey - B.S. Computer Engineering</p>
-            <p class="subtitle">August 2016 - February 2018</p>
+            <p class="title">Tecnológico de Monterrey — B.S. Computer Engineering</p>
+            <p class="subtitle">August 2016 — February 2018</p>
         </div>
     </section>
     <section>
         <h3>Certifications</h3>
         <div>
-            <a class="title" href="/Android-Mobile-Apprenticeship-Program-Certificate.pdf" target="_blank">Android Mobile Apprenticeship Program - Wizeline Academy</a>
+            <a class="title" href="/Android-Mobile-Apprenticeship-Program-Certificate.pdf" target="_blank">Android
+                Mobile Apprenticeship Program — Wizeline Academy</a>
             <p class="subtitle">Issued February 2021</p>
         </div>
         <div class="entry">
-            <a class="title" href="https://www.credly.com/badges/e5d552e0-f640-47d5-887d-04d72310b5ef" target="_blank">Prof G Certified Brand Strategist - Section4</a>
+            <a class="title" href="https://www.credly.com/badges/e5d552e0-f640-47d5-887d-04d72310b5ef" target="_blank">Prof
+                G Certified Brand Strategist — Section4</a>
             <p class="subtitle">Issued November 2020</p>
         </div>
         <div class="entry">
-            <a class="title" href="https://www.credly.com/badges/a8ff6ff5-09b2-4312-adde-aacda0ced0ea" target="_blank">Prof G Certified Business Strategist - Section4</a>
+            <a class="title" href="https://www.credly.com/badges/a8ff6ff5-09b2-4312-adde-aacda0ced0ea" target="_blank">Prof
+                G Certified Business Strategist — Section4</a>
             <p class="subtitle">Issued June 2020</p>
         </div>
         <div class="entry">
-            <a class="title" href="https://graduation.udacity.com/confirm/K7J5726R" target="_blank">Data Analyst - Udacity</a>
+            <a class="title" href="https://graduation.udacity.com/confirm/K7J5726R" target="_blank">Data Analyst —
+                Udacity</a>
             <p class="subtitle">Issued March 2019</p>
         </div>
         <div class="entry">
-            <a class="title" href="https://graduation.udacity.com/confirm/CCA3ZYTA" target="_blank">Data Foundations - Udacity</a>
+            <a class="title" href="https://graduation.udacity.com/confirm/CCA3ZYTA" target="_blank">Data Foundations —
+                Udacity</a>
             <p class="subtitle">Issued August 2018</p>
+        </div>
+    </section>
+    <section>
+        <h3>Favorite Books</h3>
+        <div class="books">
+            <a href="https://www.amazon.com.mx/Ego-Enemy-Ryan-Holiday/dp/1591847818/"><img
+                    src="/books/ego-is-the-enemy.jpg" alt="Ego Is The Enemy"></a>
+            <a href="https://www.amazon.com.mx/Extreme-Programming-Explained-Embrace-Change/dp/0321278658"><img
+                    src="/books/xp.jpg" alt="Extreme Programming Explained"></a>
+            <a href="https://www.amazon.com.mx/Algebra-Happiness-Pursuit-Success-Meaning/dp/0593084195"><img
+                    src="/books/algebra-of-happiness.jpg" alt="Algebra of Happiness"></a>
+            <a href="https://www.amazon.com.mx/Atomic-Habits-Proven-Build-Break/dp/0735211299"><img
+                    src="/books/atomic-habits.jpg" alt="Atomic Habits"></a>
+            <a href="https://www.amazon.com.mx/Measure-What-Matters-Google-Foundation/dp/0525536221"><img
+                    src="/books/what-matters.jpg" alt="Measure What Matters"></a>
+            <a href="https://www.amazon.com.mx/Psychology-Money-Timeless-Lessons-Happiness/dp/0857197681"><img
+                    src="/books/psychology-of-money.jpg"
+                    alt="The Psychology of Money: Timeless lessons on wealth, greed, and happiness"></a>
         </div>
     </section>
 </main>
 
-<p class="footer">with ❤️ from daniel</p>
+<p class="footer">❤️ from daniel</p>
 
 <style>
 
@@ -116,16 +160,61 @@
     }
 
     :global(body) {
-        font-family: system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji
+        font-family: "SF Pro Display", system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji
+    }
+
+    nav {
+        width: 100%;
+        position: fixed;
+        background-color: white;
+        z-index: 10;
+        display: grid;
+        grid-template-columns: 48px 1fr auto auto auto auto;
+        border-bottom: #00000014 solid 3px;
+        padding: 18px 32px;
+        grid-column-gap: 24px;
+        align-items: center;
+        box-sizing: border-box;
+    }
+
+    .face {
+        width: 48px;
+        height: 48px;
+        margin: 0 auto 0;
+        display: block;
+    }
+
+    .name {
+        text-transform: uppercase;
+        font-weight: 600;
+    }
+
+    .hero {
+        height: 100vh;
+        position: relative;
+    }
+
+    .gradient-text {
+        background: -webkit-linear-gradient(72deg, #7549F2, #DF58D2, #ED787C);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+
+    .summary {
+        font-weight: 500;
+        text-align: left;
+        margin: 80px 48px;
+        font-size: 4.3vw;
+        position: absolute;
+        bottom: 0;
     }
 
     h1 {
-        color: #FF6673;
         font-size: 24px;
         text-align: center;
-        margin-top: 72px;
-        font-weight: 700;
+        margin-top: 24px;
         margin-bottom: 2px;
+        font-weight: 700;
     }
 
     h2 {
@@ -150,6 +239,16 @@
         text-decoration: underline;
     }
 
+    li a {
+        color: #ED787C;
+        text-decoration: underline;
+    }
+
+    li a:hover {
+        color: #ED787C;
+        text-decoration: underline;
+    }
+
     .title {
         font-size: 18px;
         font-weight: 500;
@@ -164,26 +263,27 @@
     }
 
     .footer {
-        font-size: 14px;
+        font-family: "SF Mono", system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji;
+        font-size: 13px;
         opacity: 43%;
         text-align: center;
         padding-bottom: 24px;
     }
 
     ul {
-        margin-top: 12px;
+        margin-top: 16px;
         margin-left: 20px;
     }
 
     li {
-        opacity: 43%;
-        margin-bottom: 8px;
+        color: #0000006E;
+        margin-bottom: 10px;
         line-height: 24px;
         size: 16px;
     }
 
     .entry {
-        margin-top: 32px;
+        margin-top: 36px;
     }
 
     main {
@@ -196,5 +296,46 @@
     section {
         margin-bottom: 64px;
     }
+
+    .books {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-column-gap: 24px;
+        grid-row-gap: 24px;
+        align-items: stretch;
+    }
+
+    div img {
+        width: 100%;
+        height: 100%;
+        box-shadow: rgba(99, 99, 99, 0.2) 0 2px 8px 0;
+        border-radius: 4px;
+        outline: 1px solid rgba(99, 99, 99, 0.2);
+        outline-offset: -1px;
+    }
+
+    @media only screen and (max-width: 767px) {
+        .link {
+            display: none;
+        }
+
+        nav {
+            grid-template-columns: repeat(2, auto);
+            grid-column-gap: 16px;
+            justify-content: center;
+        }
+
+        .hero {
+            height: auto;
+        }
+
+        .summary {
+            position: relative;
+            font-size: 2em;
+            padding: 128px 32px 0;
+            margin: 0;
+        }
+    }
+
 
 </style>
